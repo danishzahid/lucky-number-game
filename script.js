5a1eb1ae21918cd6103ed5b62524fcbf1a726b92
Danish Zahid <danishst.johns@gmail.com>
2022-10-04 14:32:59 +0530

    Logic Done

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number");
 
-checkNumberButton.addEventListener('click', function getValues(){
-    console.log(dateOfBirth.value, luckyNumber.value);
+function sumOfDate(date){
+    date = date.replaceAll("-", "");
+    let sum = 0;
+    for(let i = 0; i< date.length; i++){
+        sum = sum + Number(date.charAt(i));
+    }
+    return sum;
+}
+
+checkNumberButton.addEventListener('click', function checkBdayLucky(){
+   const date = dateOfBirth.value;
+   const num = luckyNumber.value;
+   const sumDOB = sumOfDate(date);
+   if(sumDOB % num == 0){
+    console.log("Bday Lucky");
+   } else{
+    console.log("Bday not lucky");
+   }
 })
 
